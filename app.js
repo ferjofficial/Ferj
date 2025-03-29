6f9ec6da344bd92d6ecda65bcf693e07825fcf3a
Ferj <ferjohnm@gmail.com>
2025-03-28 20:41:34 -0700

    k
    k

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
